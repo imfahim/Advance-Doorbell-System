1b27df8fba23d5329377c79452b3d278252b8a19
Fahim Khan <fahim@brainstation-23.com>
2023-03-19 23:58:03 -0400

    Initiate

---
+++ SystemForm/ArduinoConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace SystemForm
+{
+    public static class ArdunioConstants
+    {
+        public const string ArduinoBoardPortName = "COM2";
+        public const int ArduinoBoardBaudRate = 9600;
+        public const string BluetoothDevice = "WearableDevice";
+        public static string Passcode = "1234";
+    }
+
+    public static class ArdunioCommands
+    {
+        public const string DoorUnlock = "U";
+        public const string LedOn = "L";
+    }
+}
+
